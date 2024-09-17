8415dae5dbbee9dcb5c33eba1ae3f25dd6f6dc4b
Leo J. <153937047+leiicamundi@users.noreply.github.com>
2024-09-17 10:36:29 +0200

    fix var file order and action generation

--- .pre-commit/docker_update_action_readmes.sh
+++ .pre-commit/docker_update_action_readmes.sh
@@ -1,15 +1,21 @@
 #!/bin/bash
 
+set -o pipefail
+
+# due to an open bug in the node image https://github.com/nodejs/docker-node/issues/740
+# we can't map the user and group at the docker level, therefore we chown the files
 USER_ID=$(id -u)
 GROUP_ID=$(id -g)
 
 # Run a single Docker container to handle the README.md updates
 docker run --rm \
+    -e USER_ID="$USER_ID" \
+    -e GROUP_ID="$GROUP_ID" \
     -v "$PWD":/workspace \
     -w /workspace \
-    -u "$USER_ID:$GROUP_ID" \
     node:22 \
     bash -c '
+        set -euxo pipefail
         npm install -g action-docs
         find .github/actions -name "*.yml" -o -name "*.yaml" | while read -r action_file; do
             action_dir=$(dirname "$action_file")
@@ -18,6 +24,14 @@ docker run --rm \
             action-docs -t 1 --no-banner -n -s "$action_file" > "$action_dir/README.md.tmp"
             # Ensure that only a single empty line is left at the end of the file
             sed -e :a -e "/^\n*\$/{\$d;N;};/\n\$/ba" "$action_dir/README.md.tmp" > "$action_dir/README.md"
+            chown "$USER_ID:$GROUP_ID" "$action_dir/README.md"
             rm -f "$action_dir/README.md.tmp"
         done
     '
+
+DOCKER_EXIT_CODE=$?
+
+if [ $DOCKER_EXIT_CODE -ne 0 ]; then
+    echo "Docker action readme generation command failed with exit code $DOCKER_EXIT_CODE, please use verbose mode"
+    exit 1
+fi
