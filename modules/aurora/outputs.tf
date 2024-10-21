48e194050c1d97dfc29164026e39d0ac9d037f1e
Leo J <153937047+leiicamundi@users.noreply.github.com>
2024-10-21 14:08:36 +0200

    fix module outputs

--- modules/aurora/outputs.tf
+++ modules/aurora/outputs.tf
@@ -4,22 +4,22 @@ output "aurora_endpoint" {
 }
 
 output "aurora_id" {
-  value       = aws_rds_cluster.id
+  value       = aws_rds_cluster.aurora_cluster.id
   description = "RDS Cluster Identifier"
 }
 
 output "aurora_resource_id" {
-  value       = aws_rds_cluster.resource_id
+  value       = aws_rds_cluster.aurora_cluster.resource_id
   description = "DB Resource Identifier"
 }
 
 output "aurora_cluster_identifier" {
-  value       = aws_rds_cluster.cluster_identifier
+  value       = aws_rds_cluster.aurora_cluster.cluster_identifier
   description = "RDS Cluster Identifier"
 }
 
 output "aurora_cluster_resource_id" {
-  value       = aws_rds_cluster.cluster_resource_id
+  value       = aws_rds_cluster.aurora_cluster.cluster_resource_id
   description = "RDS Cluster Resource ID"
 }
 
