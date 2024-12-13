38a465117b8be0b213ad794cc62b2f45823fb7d9
Balazs Kenez <96087935+bkenez@users.noreply.github.com>
2024-12-13 11:59:46 +0100

    fix: update values for c8.6

--- examples/camunda-8.6/cluster.tf
+++ examples/camunda-8.6/cluster.tf
@@ -1,10 +1,10 @@
 locals {
-  eks_cluster_name   = "cluster-name-std" # Change this to a name of your choice
-  eks_cluster_region = "eu-west-2"        # Change this to your desired AWS region
+  eks_cluster_name   = "bkenez-c86-test-cluster" # Change this to a name of your choice
+  eks_cluster_region = "eu-north-1"        # Change this to your desired AWS region
 }
 
 module "eks_cluster" {
-  source = "git::https://github.com/camunda/camunda-tf-eks-module//modules/eks-cluster?ref=3.1.1"
+  source = "git::https://github.com/camunda/camunda-tf-eks-module/tree/bkenez-test/modules/eks-cluster?ref=3.1.1"
 
   name   = local.eks_cluster_name
   region = local.eks_cluster_region
