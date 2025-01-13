f031a3643e5c92984602c2022cc88ce3d32eb2e9
Balazs Kenez <96087935+bkenez@users.noreply.github.com>
2025-01-13 18:58:51 +0100

    fix: pre-commit trivy script was missing path

--- .lint/trivy/trivy-scan.sh
+++ .lint/trivy/trivy-scan.sh
@@ -1,3 +1,2 @@
-# Since Trivy does not have a pre-commit hook by default, this is a custom hook script
 #!/bin/bash
-trivy config --config .lint/trivy/trivy.yaml --ignorefile .trivyignore
+trivy config --config .lint/trivy/trivy.yaml --ignorefile .trivyignore .
