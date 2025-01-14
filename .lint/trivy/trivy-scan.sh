781cd37e44128a281a05e88e7e8fc9c3ed03ac1c
Balazs Kenez <96087935+bkenez@users.noreply.github.com>
2025-01-14 13:48:09 +0100

    fix: revert previous change

--- .lint/trivy/trivy-scan.sh
+++ .lint/trivy/trivy-scan.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 cd "$(git rev-parse --show-toplevel)"
-trivy config --config .lint/trivy/trivy.yaml --ignorefile .trivyignore .
+trivy config --config .lint/trivy/trivy.yaml --ignorefile .trivyignore ./examples/camunda-8.6
+trivy config --config .lint/trivy/trivy.yaml --ignorefile .trivyignore ./examples/camunda-8.6-irsa
+trivy config --config .lint/trivy/trivy.yaml --ignorefile .trivyignore ./examples/camunda-8.7
+trivy config --config .lint/trivy/trivy.yaml --ignorefile .trivyignore ./examples/camunda-8.7-irsa
+trivy config --config .lint/trivy/trivy.yaml --ignorefile .trivyignore ./modules
