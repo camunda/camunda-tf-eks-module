316ad05cb88de5efba3aab4d549c6dd2238959d0
Balazs Kenez <96087935+bkenez@users.noreply.github.com>
2025-01-14 11:57:48 +0100

    fix: modify trivy in pre-commit hook as an attempt to fix CICD behaviour

--- .lint/trivy/trivy-scan.sh
+++ .lint/trivy/trivy-scan.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
-trivy config --config .lint/trivy/trivy.yaml --ignorefile .trivyignore .
+trivy config --config .lint/trivy/trivy.yaml --ignorefile .trivyignore ./examples/camunda-8.6
+trivy config --config .lint/trivy/trivy.yaml --ignorefile .trivyignore ./examples/camunda-8.6-irsa
+trivy config --config .lint/trivy/trivy.yaml --ignorefile .trivyignore ./examples/camunda-8.7
+trivy config --config .lint/trivy/trivy.yaml --ignorefile .trivyignore ./examples/camunda-8.7-irsa
+trivy config --config .lint/trivy/trivy.yaml --ignorefile .trivyignore ./modules
