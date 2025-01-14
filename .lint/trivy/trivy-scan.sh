ee4732c927e213bb99d856c2b7e5e0f0972fdfca
Balazs Kenez <96087935+bkenez@users.noreply.github.com>
2025-01-14 13:40:59 +0100

    fix: try to specify working directory

--- .lint/trivy/trivy-scan.sh
+++ .lint/trivy/trivy-scan.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
-trivy config --config .lint/trivy/trivy.yaml --ignorefile .trivyignore ./examples/camunda-8.6
-trivy config --config .lint/trivy/trivy.yaml --ignorefile .trivyignore ./examples/camunda-8.6-irsa
-trivy config --config .lint/trivy/trivy.yaml --ignorefile .trivyignore ./examples/camunda-8.7
-trivy config --config .lint/trivy/trivy.yaml --ignorefile .trivyignore ./examples/camunda-8.7-irsa
-trivy config --config .lint/trivy/trivy.yaml --ignorefile .trivyignore ./modules
+cd "$(git rev-parse --show-toplevel)"
+trivy config --config .lint/trivy/trivy.yaml --ignorefile .trivyignore .
