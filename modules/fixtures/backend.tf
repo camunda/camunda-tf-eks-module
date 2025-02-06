3d6b87546fe8ad01cd0cfd916dcc2326d04bae6c
Leo J. <153937047+leiicamundi@users.noreply.github.com>
2025-02-06 10:36:47 +0100

    fix(modules): add required_providers in modules (#224)
    The required_providers is a specification required to ensure that a
    module defines its dependencies.
    This PR fixes the absence.

--- modules/fixtures/backend.tf
+++ modules/fixtures/backend.tf
@@ -1,14 +1,5 @@
 # this file is used to declare a backend used during the tests
 
 terraform {
-  required_version = ">= 1.0"
-
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = ">= 5.35.0"
-    }
-  }
-
   backend "s3" {}
 }
